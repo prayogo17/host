fa6d22ab90e4af6d12660c0f7c34291a12cd4f4e
prayogo17 <nugrohoprayogo97@gmail.com>
2017-08-11 22:04:39 +0700

    Update blog.js

--- blog.js
+++ blog.js
@@ -63,8 +63,8 @@ else {
 }
 function fontsize(){
 if ($(window).width() < 569) {
- var k=568/140;
- k=($(window).width()/k)*0.1;
+ var k=568/70;
+ k=($(window).width()/k)*0.2;
 	//console.log(k);
 $('#menustatic').css({'font-size': k+'px'});
 }

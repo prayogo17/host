27ea01111ea5648fab355000e8fbe5c138232b4b
prayogo17 <nugrohoprayogo97@gmail.com>
2017-08-22 23:40:31 +0700

    Update blog.js

--- blog.js
+++ blog.js
@@ -123,7 +123,7 @@ function contains(a,e){
 function printRelatedLabels(){
 	var r=Math.floor((relatedTitles.length-1)*Math.random());
 	var i=0;document.write('<ul id="terkait">');
-	while(i<relatedTitles.length&&i<20)
+	while(i<6&&i<20)
 	{
 		var y=relatedImg[r].replace(/\\/g, '');
 	          y=y.replace("s72-c", "s125");

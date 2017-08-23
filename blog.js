4f6f2003595399ee3cf782346f50e1fc2bf78fdf
prayogo17 <nugrohoprayogo97@gmail.com>
2017-08-23 09:28:46 +0700

    Update blog.js

--- blog.js
+++ blog.js
@@ -84,11 +84,13 @@ var relatedTitles=new Array();
 var relatedTitlesNum=0;
 var relatedUrls=new Array();
 var relatedImg=new Array();
+var relatedDesk=new Array();
 function related_results_labels(json){
 	for(var i=0;i<json.feed.entry.length;i++){
 		var entry=json.feed.entry[i];
 		relatedTitles[relatedTitlesNum]=entry.title.$t;
 		relatedImg[relatedTitlesNum]=entry.media$thumbnail.url;
+		relatedDesk[relatedTitlesNum]=entry.summary.$t;
 		for(var k=0;k<entry.link.length;k++){
 			if(entry.link[k].rel=='alternate')
 			{
@@ -103,16 +105,19 @@ function removeRelatedDuplicates(){
 	var tmp=new Array(0);
 	var tmp2=new Array(0);
 	var tmp3=new Array(0);
+	var tmp4=new Array(0);
 	for(var i=0;i<relatedUrls.length;i++){
 		if(!contains(tmp,relatedUrls[i])){
 			tmp.length+=1;tmp[tmp.length-1]=relatedUrls[i];
 			tmp2.length+=1;tmp2[tmp2.length-1]=relatedTitles[i];
 			tmp3.length+=1;tmp3[tmp3.length-1]=relatedImg[i];
+			tmp4.length+=1;tmp4[tmp4.length-1]=relatedDesk[i];
 		}
 	}
 relatedTitles=tmp2;
 	relatedUrls=tmp;
 	relatedImg=tmp3;
+	relatedDesk=tmp4;
 }
 function contains(a,e){
 	for(var j=0;j<a.length;j++)
@@ -126,8 +131,11 @@ function printRelatedLabels(){
 	while(i<relatedTitles.length&&i<6)
 	{
 		var y=relatedImg[r].replace(/\\/g, '');
-	          y=y.replace("s72-c", "s125");
-		document.write('<li><img src='+y+' height="100" width="100"/>  <a href='+relatedUrls[r]+'>'+relatedTitles[r]+'</a></li>');
+	          y=y.replace("s72-c", "s100");
+		var des=relatedDesk[r].replace(/\n/g,'');
+		
+		//document.write('<li><img src='+y+' height="100" width="100"/>  <a href='+relatedUrls[r]+'>'+relatedTitles[r]+'</a></li>');
+		
 					    if(r<relatedTitles.length-1){r++;
 					    }else{
 						    r=0;

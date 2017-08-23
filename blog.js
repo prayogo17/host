50a18bf4032d28f266988ae0ddbb9f6c3e36f142
prayogo17 <nugrohoprayogo97@gmail.com>
2017-08-24 06:48:12 +0700

    Update blog.js

--- blog.js
+++ blog.js
@@ -145,3 +145,28 @@ i++;
 	}
 document.write('</ul>');document.write();
 }
+$(document).on('click','#pp',function(){
+    $('.isi1').show();
+    $('.isi2').hide();
+    $('.isi3').hide();
+   
+    $('.active').removeClass('active');
+     $(this).addClass('active');
+    
+});
+$(document).on('click','#ll',function(){
+    $('.isi1').hide();
+    $('.isi2').show();
+    $('.isi3').hide();
+    
+    $('.active').removeClass('active');
+    $(this).addClass('active');
+});
+$(document).on('click','#aa',function(){
+    $('.isi1').hide();
+    $('.isi2').hide();
+    $('.isi3').show();
+   
+    $('.active').removeClass('active');
+     $(this).addClass('active');
+});

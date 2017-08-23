f8ef540e3e5a86d62691f2ff3588fdea84b52d8f
prayogo17 <nugrohoprayogo97@gmail.com>
2017-08-23 10:26:17 +0700

    Update blog.js

--- blog.js
+++ blog.js
@@ -133,6 +133,7 @@ function printRelatedLabels(){
 		var y=relatedImg[r].replace(/\\/g, '');
 	          y=y.replace("s72-c", "s100");
 		var des=relatedDesk[r].replace(/\n/g,'');
+		des=des.substr(0,235);
 		
 		//document.write('<li><img src='+y+' height="100" width="100"/>  <a href='+relatedUrls[r]+'>'+relatedTitles[r]+'</a></li>');
 		document.write('<div class="kait"><a href='+relatedUrls[r]+'><img src='+y+' alt='+relatedTitles[r]+'></a><a href='+relatedUrls[r]+'><h4>'+relatedTitles[r]+'</h4></a><p>'+des+'</p></div>');

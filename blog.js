e831694a83718931e954eb86bc3c8bab0f563fe2
prayogo17 <nugrohoprayogo97@gmail.com>
2017-12-01 09:44:46 +0700

    Update blog.js

--- blog.js
+++ blog.js
@@ -176,7 +176,7 @@ $(document).on('click','#aa',function(){
 window.onscroll= function(){artikel_lain()};
 function artikel_lain(){
 
-    if($('#buka').isOnScreen()||$('#tutup').isOnScreen()){
+    if($('#sidebar1').isOnScreen()||$('footer').isOnScreen()){
         
           $("#kotakfix").css("transform", "translateX(100%)"); 
     }else{
